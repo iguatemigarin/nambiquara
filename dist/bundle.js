b0d17e2b5fc35712f598e45c01fad21c4066f89d
Iguatemi Garin <iguatemi@gmail.com>
2018-08-17 15:26:07 -0300

    es5

--- dist/bundle.js
+++ dist/bundle.js
@@ -4,51 +4,51 @@
     (factory((global.nambiquara = {})));
 }(this, (function (exports) { 'use strict';
 
-    const makeHeader = () => `<?xml version="1.0" encoding="UTF-8"?><?mso-application progid="Excel.Sheet"?>`;
+    var makeHeader = function () { return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><?mso-application progid=\"Excel.Sheet\"?>"; };
     // tslint:disable
-    const makeWorkbookOpenTag = () => `<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:c="urn:schemas-microsoft-com:office:component:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:x2="http://schemas.microsoft.com/office/excel/2003/xml" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">`;
+    var makeWorkbookOpenTag = function () { return "<Workbook xmlns=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:c=\"urn:schemas-microsoft-com:office:component:spreadsheet\" xmlns:html=\"http://www.w3.org/TR/REC-html40\" xmlns:o=\"urn:schemas-microsoft-com:office:office\" xmlns:ss=\"urn:schemas-microsoft-com:office:spreadsheet\" xmlns:x2=\"http://schemas.microsoft.com/office/excel/2003/xml\" xmlns:x=\"urn:schemas-microsoft-com:office:excel\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"; };
     // tslint:enable
-    const makeWorkbookCloseTag = () => `</Workbook>`;
-    const makeWorksheetOpenTag = () => `<ss:Worksheet ss:Name="Worksheet1">`;
-    const makeWorksheetCloseTag = () => `</ss:Worksheet>`;
-    const makeWorksheetOptionsTag = () => `<x:WorksheetOptions/>`;
-    const makeTableOpenTag = () => `<Table>`;
-    const makeTableCloseTag = () => `</Table>`;
-    const makeRowOpenTag = () => `<Row>`;
-    const makeRowCloseTag = () => `</Row>`;
-    const makeCellOpenTag = () => `<Cell>`;
-    const makeCellCloseTag = () => `</Cell>`;
-    const makeTypeForValue = (value) => {
+    var makeWorkbookCloseTag = function () { return "</Workbook>"; };
+    var makeWorksheetOpenTag = function () { return "<ss:Worksheet ss:Name=\"Worksheet1\">"; };
+    var makeWorksheetCloseTag = function () { return "</ss:Worksheet>"; };
+    var makeWorksheetOptionsTag = function () { return "<x:WorksheetOptions/>"; };
+    var makeTableOpenTag = function () { return "<Table>"; };
+    var makeTableCloseTag = function () { return "</Table>"; };
+    var makeRowOpenTag = function () { return "<Row>"; };
+    var makeRowCloseTag = function () { return "</Row>"; };
+    var makeCellOpenTag = function () { return "<Cell>"; };
+    var makeCellCloseTag = function () { return "</Cell>"; };
+    var makeTypeForValue = function (value) {
         switch (typeof value) {
             case 'string': return 'String';
             case 'number': return 'Number';
             default: return 'String';
         }
     };
-    const makeDataOpenTag = (value) => `<Data ss:Type="${makeTypeForValue(value)}">`;
-    const makeDataCloseTag = () => `</Data>`;
-    const makeValue = (value) => value == null ? '' : value;
-    const makeDataTag = (value) => `${makeDataOpenTag(value)}${makeValue(value)}${makeDataCloseTag()}`;
-    const makeCellTag = (value) => `${makeCellOpenTag()}${makeDataTag(value)}${makeCellCloseTag()}`;
-    const makeRow = (values) => `${makeRowOpenTag()}${values.map(makeCellTag).join('')}${makeRowCloseTag()}`;
-    const makeRows = (values) => values.map(makeRow).join('');
-    const makeTableTag = (values) => `${makeTableOpenTag()}${makeRows(values)}${makeTableCloseTag()}`;
-    const makeWorksheet = (values) => [
+    var makeDataOpenTag = function (value) { return "<Data ss:Type=\"" + makeTypeForValue(value) + "\">"; };
+    var makeDataCloseTag = function () { return "</Data>"; };
+    var makeValue = function (value) { return value == null ? '' : value; };
+    var makeDataTag = function (value) { return "" + makeDataOpenTag(value) + makeValue(value) + makeDataCloseTag(); };
+    var makeCellTag = function (value) { return "" + makeCellOpenTag() + makeDataTag(value) + makeCellCloseTag(); };
+    var makeRow = function (values) { return "" + makeRowOpenTag() + values.map(makeCellTag).join('') + makeRowCloseTag(); };
+    var makeRows = function (values) { return values.map(makeRow).join(''); };
+    var makeTableTag = function (values) { return "" + makeTableOpenTag() + makeRows(values) + makeTableCloseTag(); };
+    var makeWorksheet = function (values) { return [
         makeWorksheetOpenTag(),
         makeTableTag(values),
         makeWorksheetOptionsTag(),
         makeWorksheetCloseTag(),
-    ].join('');
-    const makeWorkBook = (values) => [
+    ].join(''); };
+    var makeWorkBook = function (values) { return [
         makeWorkbookOpenTag(),
         makeWorksheet(values),
         makeWorkbookCloseTag(),
-    ].join('');
-    const makeSheet = (values) => [
+    ].join(''); };
+    var makeSheet = function (values) { return [
         makeHeader(),
         makeWorkBook(values),
-    ].join('');
-    const fromArray = (values) => makeSheet(values);
+    ].join(''); };
+    var fromArray = function (values) { return makeSheet(values); };
 
     exports.fromArray = fromArray;
 
